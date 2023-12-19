cae3d6353c6909d40dffb8f93acbac4a3ed76ab7
Antonio Roberts <141260347+evroyr@users.noreply.github.com>
2023-12-18 19:40:47 -0500

    rende

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ function render(state = store.Home) {
     ${Footer()}
   `;
   router.updatePageLinks();
+  afterRender();
+}
+
+function afterRender() {
+  // add menu toggle to bars icon in nav bar
+  document.querySelector(".fa-bars").addEventListener("click", () => {
+    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  });
 }
 
 router
@@ -29,8 +37,3 @@ router
     }
   })
   .resolve();
-
-// add menu toggle to bars icon in nav bar
-//document.querySelector(".fa-bars").addEventListener("click", () => {
-//document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-//});
